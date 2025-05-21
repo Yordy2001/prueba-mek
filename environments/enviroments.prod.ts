2256c9a679f492ee8187b3daef7eaa8b594ff239
Yordy2001 <kingyordi2001@gmail.com>
2025-05-21 12:47:28 -0400

    config to deploy

---
+++ environments/enviroments.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://prueba-mek-api-production.up.railway.app', // URL del backend en local
+    enableDebug: true, // Bandera para depuración
+};
--- environments/environments.ts
+++ environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000', // URL del backend en local
+    apiUrl: 'prueba-mek-api-production.up.railway.app', // URL del backend en local
     enableDebug: true, // Bandera para depuración
 };
